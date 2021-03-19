ec890a15bec2efb78038357c157bcf89e70f8683
VictorSilva15 <victor.silvaPC@hotmail.com>
2021-03-19 14:08:11 -0300

    project finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import icon from "./imgs/dinheiro-icon.png";
+import github from "./imgs/github.png";
+import "./App.css";
+import Conversor from "./components/Conversor";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={icon} className="Icon-money" alt="money" />
+        <h1>Conversor de moeda</h1>
       </header>
+      <section id="content">
+        <div className="block">
+          <Conversor moedaA="BRL" moedaB="USD"></Conversor>
+        </div>
+        <div className="block">
+          <Conversor moedaA="USD" moedaB="BRL"></Conversor>
+        </div>
+        <div className="block">
+          <Conversor moedaA="BRL" moedaB="CAD"></Conversor>
+        </div>
+        <div className="block">
+          <Conversor moedaA="CAD" moedaB="BRL"></Conversor>
+        </div>
+        <div className="block">
+          <Conversor moedaA="BRL" moedaB="JPY"></Conversor>
+        </div>
+        <div className="block">
+          <Conversor moedaA="JPY" moedaB="BRL"></Conversor>
+        </div>
+        <div className="block">
+          <Conversor moedaA="BRL" moedaB="EUR"></Conversor>
+        </div>
+        <div className="block">
+          <Conversor moedaA="EUR" moedaB="BRL"></Conversor>
+        </div>
+      </section>
+      <div className="footer">
+        <img src={github} className="criador" alt="criador" />
+        <a
+          href="https://github.com/VictorSilva15"
+          rel="noreferrer"
+          target="_blank"
+        >
+          VictorSilva15
+        </a>
+      </div>
     </div>
   );
 }
